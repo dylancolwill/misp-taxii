d4ee5f26f1d75f863ebffd8e945b5142c5850482
dylancolwill <dylan.colwill@hotmail.com>
2025-09-15 13:40:17 +1000

    cleaning

--- endpoints/discovery.py
+++ endpoints/discovery.py
@@ -15,9 +15,6 @@ discovery_info= {
 @router.get("/taxii2/", tags=["Discovery"])
 async def get_discovery():
     """
-    TAXII 2.1 Discovery Endpoint
-    ---
-    Returns a list of API roots available on this server.
-    First endpoint client calls to discover the server.
+    returns list of api endpoints on server
     """
     return discovery_info
