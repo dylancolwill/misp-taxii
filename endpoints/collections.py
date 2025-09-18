7ca87ddfb3109ba193aed16b83180c2f0c399f97
dylancolwill <dylan.colwill@hotmail.com>
2025-09-18 15:55:20 +1000

    add functionality for search by collection id

--- endpoints/collections.py
+++ endpoints/collections.py
@@ -20,6 +20,7 @@ def get_misp_collections(headers= None):
         raise RuntimeError(f'MISP request failed: {e}')
 
     can_write=misp.get_user_perms(headers=headers) #get the user perms from function in core
+    
     collections = []
     for tag in tags:
         collections.append({
@@ -33,3 +34,33 @@ def get_misp_collections(headers= None):
     
     return collections
 
+@router.get('/taxii2/api1/collections/{collection_id}', tags=['Collections'])
+def get_misp_collections(collection_id = int, headers= None):
+    """
+    Fetch all MISP tags and convert them into TAXII 2.1 collections.
+    Each tag becomes a collection.
+    """
+    print('start')
+    try:
+        response = misp.query_misp_api('/tags/index', headers=headers)
+        # response.raise_for_status()
+        print(response)
+        tags = response.get('Tag')  #returns a list of tag dicts
+    except requests.exceptions.RequestException as e:
+        raise RuntimeError(f'MISP request failed: {e}')
+
+    can_write=misp.get_user_perms(headers=headers) #get the user perms from function in core
+    
+    # find matching tag
+    tag = next((t for t in tags if str(t['id']) == collection_id), None)
+    if not tag:
+        raise HTTPException(status_code=404, detail='Collection not found')
+    
+    return {
+        'id': tag['id'],
+        'title': tag['name'],
+        'description': tag.get('description', None),
+        'can_read': True, #if user got this far through request, can access
+        'can_write': can_write,
+        'media_types': ['application/stix+json;version=2.1']
+    }
