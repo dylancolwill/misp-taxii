6b56ffb40af1fdd46ee8b1439cbf18aa0815ad90
dylancolwill <dylan.colwill@hotmail.com>
2025-09-18 15:20:24 +1000

    accurate testing

--- tests/collections_test.py
+++ tests/collections_test.py
@@ -1,23 +1,39 @@
-from functions import misp
+# from functions import misp
 
-print('start')
+# print('start')
 
+# auth="w35U4gMEzUl9TBox9kQcOQStIdAb4emdK1SoIY8K"
+
+# headers = {
+#             "Authorization": auth,
+#             "Accept": "application/json",
+#             "Content-Type": "application/json"
+#         }
+
+# data = None
+
+# print('querymisp')
+
+# try:
+#     data = misp.query_misp_api("/tags/index", headers=headers)
+# except Exception as e:
+#     print("Error:", e)
+    
+# print('end')
+# print(data)
+
+
+import requests
+
+url = "http://127.0.0.1:8000/taxii2/api1/collections/"
 auth="w35U4gMEzUl9TBox9kQcOQStIdAb4emdK1SoIY8K"
 
 headers = {
             "Authorization": auth,
-            "Accept": "application/json",
+            "Accept": "application/taxii+json;version=2.1",
             "Content-Type": "application/json"
         }
 
-data = None
-
-print('querymisp')
-
-try:
-    data = misp.query_misp_api("/tags/index", headers=headers)
-except Exception as e:
-    print("Error:", e)
-    
-print('end')
-print(data)
+resp = requests.get(url, headers=headers)
+# print(resp.status_code)
+print(resp.json())
