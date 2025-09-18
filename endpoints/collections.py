42a9243ed7f498dbacd09bb4971e6a3ec3da11e1
dylancolwill <dylan.colwill@hotmail.com>
2025-09-18 18:05:22 +1000

    small optimising changes

--- endpoints/collections.py
+++ endpoints/collections.py
@@ -12,14 +12,9 @@ def get_misp_collections(headers= None):
     Fetch all MISP tags and convert them into TAXII 2.1 collections.
     Each tag becomes a collection.
     """
-    print('start')
-    try:
-        response = misp.query_misp_api('/tags/index', headers=headers)
-        # response.raise_for_status()
-        print(response)
-        tags = response.get('Tag')  #returns a list of tag dicts
-    except requests.exceptions.RequestException as e:
-        raise RuntimeError(f'MISP request failed: {e}')
+    print('getting all misp tags...')
+    response = misp.query_misp_api('/tags/index', headers=headers)
+    tags = response.get('Tag')  #returns a list of tag dicts
 
     can_write=misp.get_user_perms(headers=headers) #get the user perms from function in core
     
@@ -36,24 +31,20 @@ def get_misp_collections(headers= None):
     
     return collections
 
-@router.get('/taxii2/api1/collections/{collection_id}', tags=['Collections'])
-def get_misp_collections(collection_id = int, headers= None):
+@router.get('/taxii2/api1/collections/{collection_uuid}', tags=['Collections'])
+def get_misp_collections(collection_uuid = int, headers= None):
     """
     since taxii requires uuid not id, need to fetch all tags and filter in code, cannot query for id
     """
-    print('start')
-    try:
-        response = misp.query_misp_api('/tags/index', headers=headers)
-        # response.raise_for_status()
-        print(response)
-        tags = response.get('Tag')  #returns a list of tag dicts
-    except requests.exceptions.RequestException as e:
-        raise RuntimeError(f'MISP request failed: {e}')
+    print('getting all misp tags...')
+    response = misp.query_misp_api('/tags/index', headers=headers)
+    tags = response.get('Tag')  #returns a list of tag dicts
 
     can_write=misp.get_user_perms(headers=headers) #get the user perms from function in core
     
     # find matching tag
-    tag = next((t for t in tags if conversion.str_to_uuid(str(t['id'])) == collection_id), None)
+    print('comparing each tag id to user inputted uuid...')
+    tag = next((t for t in tags if conversion.str_to_uuid(str(t['id'])) == collection_uuid), None)
     if not tag:
         raise HTTPException(status_code=404, detail='Collection not found')
     
