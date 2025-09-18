47f15832af87a6ec578f48b6f3ce7ed050022853
dylancolwill <dylan.colwill@hotmail.com>
2025-09-18 15:26:24 +1000

    accurate testing

--- tests/collections_test.py
+++ tests/collections_test.py
@@ -24,12 +24,13 @@
 
 
 import requests
+import creds
 
 url = "http://127.0.0.1:8000/taxii2/api1/collections/"
 auth="w35U4gMEzUl9TBox9kQcOQStIdAb4emdK1SoIY8K"
 
 headers = {
-            "Authorization": auth,
+            "Authorization": creds.get_creds(),
             "Accept": "application/taxii+json;version=2.1",
             "Content-Type": "application/json"
         }
