73e899886fa305aa12e0d7c8a6582fee43c545a2
dylancolwill <dylan.colwill@hotmail.com>
2025-09-19 15:21:36 +1000

    included filtering and required response headers by speec

--- endpoints/manifests.py
+++ endpoints/manifests.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request, HTTPException
+from fastapi import APIRouter, Depends, Request, HTTPException, Query, Response
 import functions.misp as misp
 import functions.conversion as conversion
 import requests
@@ -6,15 +6,26 @@ import requests
 router = APIRouter()
 
 @router.get('/taxii2/{api_root}/collections/{collection_uuid}/manifests', tags=['Manifests'])
-def get_misp_collections(collection_uuid: str, request: Request):
+def get_misp_manifests(collection_uuid: str,
+    added_after: str = Query(None),
+    limit: int = Query(None),
+    next_token: str = Query(None), #taxii next
+    object_id: str = Query(None, alias='match[id]'),
+    object_type: str = Query(None, alias='match[type]'),
+    version: str = Query(None, alias='match[version]'),
+    spec_version: str = Query(None, alias='match[spec_version]'),
+    request: Request = None,
+    response: Response = None
+):
     """
     since taxii requires uuid not id, need to fetch all tags and filter in code, cannot query for id
     """
+    
     headers = dict(request.headers)
     
     print('getting all misp tags...')
-    response = misp.query_misp_api('/tags/index', headers=headers)
-    tags = response.get('Tag')  #returns a list of tag dicts
+    misp_response = misp.query_misp_api('/tags/index', headers=headers)
+    tags = misp_response.get('Tag')  #returns a list of tag dicts
     
     # find matching tag, need to convert each collection id to uuid
     print('comparing each tag id to user inputted uuid...')
@@ -26,14 +37,24 @@ def get_misp_collections(collection_uuid: str, request: Request):
     
     print('getting related misp events...')
     payload = {
-        "tags": collection_name,
-        "returnFormat": "json"
+        'tags': collection_name,
+        'returnFormat': 'json'
     }
-    response = misp.query_misp_api('/events/restSearch', method="POST",  headers=headers, data=payload)
-    events=response['response']
+    
+    if added_after:
+        payload['date_from'] = added_after
+    if limit:
+        payload['limit'] = limit
+    if next_token:
+        payload['page'] = int(next_token) 
+    
+    misp_response = misp.query_misp_api('/events/restSearch', method='POST',  headers=headers, data=payload)
+    events=misp_response['response']
     
     manifests = []
+    date_added_list = []
     for event in events:
+        date_added_list.append(event['Event']['date']) #prepare for response header required in taxii spec
         manifests.append({
             'id': event['Event']['uuid'], #NEED TO INCLUDE STIX OBJECT TYPE
             'date_added': event['Event']['date'],
@@ -42,5 +63,9 @@ def get_misp_collections(collection_uuid: str, request: Request):
         })
         
     print('complete')
+    
+    if date_added_list:
+        response.headers['X-TAXII-Date-Added-First'] = min(date_added_list)
+        response.headers['X-TAXII-Date-Added-Last'] = max(date_added_list)
     
     return {'objects':manifests}
