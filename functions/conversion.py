1a63ded48132f313ce601b449c93cde8f4f06879
Lewis <perrylfc@gmail.com>
2025-09-19 16:23:04 +1000

    initial commit

--- functions/conversion.py
+++ functions/conversion.py
@@ -1,10 +1,11 @@
+from misp_stix_converter import MISPtoSTIX21Parser
+from stix2 import parse
+
 import uuid
 
 def str_to_uuid(string):
     return str(uuid.uuid5(uuid.NAMESPACE_OID, str(string)))
 
-from misp_stix_converter import MISPtoSTIX21Parser
-from stix2 import parse
 
 def misp_to_stix(event):
     # takes misp event converts to json
