a4858070fe2f881f3eddefdb0216c43ad8e27537
dylancolwill <dylan.colwill@hotmail.com>
2025-09-26 16:38:51 +1000

    added comments

--- endpoints/collections.py
+++ endpoints/collections.py
@@ -4,6 +4,7 @@ import functions.conversion as conversion
 import requests
 import uuid
 
+# api router instance to use with fastapi server
 router = APIRouter()
 
 @router.get('/taxii2/{api_root}/collections/', tags=['Collections'])
@@ -12,20 +13,24 @@ def get_misp_collections(request: Request = None, response: Response = None):
     Fetch all MISP tags and convert them into TAXII 2.1 collections.
     Each tag becomes a collection.
     """
+    # extract headers from initial request
     headers = dict(request.headers)
     print(headers)
     
+    # query misp for all tags using headers
     print('getting all misp tags...')
     misp_response = misp.query_misp_api('/tags/index', headers=headers)
     tags = misp_response.get('Tag')  #returns a list of tag dicts
 
+    # determine user permissions from misp
     print('getting user perms...')
-    can_write=misp.get_user_perms(headers=headers) #get the user perms from function in core
+    can_write=misp.get_user_perms(headers=headers) #function to check write access
     
+    # convert each misp tag into taxii collection object
     collections = []
     for tag in tags:
         collections.append({
-            'id': conversion.str_to_uuid(tag['id']), #convert id to uuid
+            'id': conversion.str_to_uuid(tag['id']), #convert id to uuid, taxii requires uuid
             'title': tag['name'],
             'description': tag.get('description', None),
             'can_read': True, #if user got this far through request, can access
@@ -33,8 +38,10 @@ def get_misp_collections(request: Request = None, response: Response = None):
             'media_types': ['application/stix+json;version=2.1']
         })
         
+    # set taxii content type in header as per specs
     response.headers['Content-Type']= 'application/taxii+json;version=2.1'
     
+    # return in format defined by specs
     return {'collections':collections}
 
 @router.get('/taxii2/{api_root}/collections/{collection_uuid}', tags=['Collections'])
@@ -42,19 +49,23 @@ def get_misp_collection(collection_uuid: str, request: Request, response: Respon
     """
     since taxii requires uuid not id, need to fetch all tags and filter in code, cannot query for id
     """
+    # extract headers from initial request
     headers = dict(request.headers)
     
+    # query misp for all tags using headers
     print('getting all misp tags...')
     misp_response = misp.query_misp_api('/tags/index', headers=headers)
     tags = misp_response.get('Tag')  #returns a list of tag dicts
 
+    # determine user permissions from misp
+    print('getting user perms...')
     can_write=misp.get_user_perms(headers=headers) #get the user perms from function in core
     
-    # find matching tag, need to convert each collection id to uuid
+    # find matching tag, converting each collection id to uuid then checking if match
     print('comparing each tag id to user inputted uuid...')
     tag = next((t for t in tags if conversion.str_to_uuid(str(t['id'])) == collection_uuid), None)
     if not tag:
-        raise HTTPException(status_code=404, detail='Collection not found')
+        raise HTTPException(status_code=404, detail='Collection not found') #error if no matching tag
     
     response.headers['Content-Type']= 'application/taxii+json;version=2.1'
     
