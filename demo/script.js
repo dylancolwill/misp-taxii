28a0abf2f24aaea86d1802434ed00f2722759143
dylancolwill <dylan.colwill@hotmail.com>
2025-10-14 18:44:22 +1100

    custom headers functionality

--- demo/script.js
+++ demo/script.js
@@ -1,21 +1,34 @@
-const headers = {
-    "Authorization": "w35U4gMEzUl9TBox9kQcOQStIdAb4emdK1SoIY8K",
-    "Accept": "application/taxii+json;version=2.1",
-    "Content-Type": "application/taxii+json;version=2.1"
-};
+function getHeaders() {
+    return {
+        "Authorization": "w35U4gMEzUl9TBox9kQcOQStIdAb4emdK1SoIY8K",
+        "Accept": "application/taxii+json;version=2.1",
+        "Content-Type": "application/taxii+json;version=2.1"
+    };
+}
+$("#headers").val(JSON.stringify(getHeaders(), null, 2));
 
-function send_request(customUrl) {
+function send_request(customUrl, method = "GET", data = null) {
     const url = customUrl || $("#requestUrl").val();
 
-     $("#outputSpace").html("loading...");
+    let headers;
+    try {
+        headers = JSON.parse($("#headers").val());
+    } catch (e) {
+        $("#outputSpace").html("<pre>invalid headers</pre>");
+        return;
+    }
+
+    $("#outputSpace").html("loading...");
     if (!url) {
         $("#outputSpace").html("<pre>url empty</pre>");
         return;
     }
     $.ajax({
         url: url,
-        method: "GET",
+        method: method,
         headers: headers,
+        data: data ? JSON.stringify(data) : undefined,
+        contentType: headers["Content-Type"],
         success: function (data) {
             $("#outputSpace").html("<pre>" + JSON.stringify(data, null, 2) + "</pre>");
         },
@@ -60,19 +73,25 @@ $("#btnListObj").click(function() {
 });
 
 $("#btnGetObj").click(function() {
-    const url = "http://127.0.0.1:8000/taxii2/api1/collections/d6ed313e-533a-55a6-aa06-4c00bc132812/objects/identity--7f4e0b2a-1f7b-41f4-9e76-a18fa1ce86e8";
+    const url = "http://127.0.0.1:8000/taxii2/api1/collections/28dfa8c5-dff4-52ad-90df-e5112b2ade90/objects/report--59e9ec59-a888-48e4-afb4-441602de0b81";
+    $("#requestUrl").val(url);
+    send_request(url);
+});
+
+$("#btnAddObj").click(function() {
+    const url = "http://127.0.0.1:8000/taxii2/api1/collections/f37b15ee-07ba-583f-bf65-03e4fd5e9d96/objects/";
     $("#requestUrl").val(url);
     send_request(url);
 });
 
 $("#btnManifests").click(function() {
-    const url = "http://127.0.0.1:8000/taxii2/api1/collections/d6ed313e-533a-55a6-aa06-4c00bc132812/manifests?limit=2";
+    const url = "http://127.0.0.1:8000/taxii2/api1/collections/28dfa8c5-dff4-52ad-90df-e5112b2ade90/manifests?limit=2";
     $("#requestUrl").val(url);
     send_request(url);
 });
 
 $("#btnVersions").click(function() {
-    const url = "http://127.0.0.1:8000/taxii2/api1/collections/d6ed313e-533a-55a6-aa06-4c00bc132812/objects/grouping--28f673f6-074f-484a-9a22-e89b786a5671/versions";
+    const url = "http://127.0.0.1:8000/taxii2/api1/collections/28dfa8c5-dff4-52ad-90df-e5112b2ade90/objects/report--59e9ec59-a888-48e4-afb4-441602de0b81/versions";
     $("#requestUrl").val(url);
     send_request(url);
 });
